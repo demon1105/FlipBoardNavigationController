7681fad25ff1ce56fb73924da9adecc7bced1914
Nicholas Tau <demon42111915@gmail.com>
2013-06-08 17:54:43 +0800

    添加新文件

---
+++ Sample/FlipViewControllerDemo/FlipViewControllerDemo/UIView+Util.h
@@ -0,0 +1,13 @@
+//
+//  UIView+Util.h
+//  FlipViewControllerDemo
+//
+//  Created by demon on 6/8/13.
+//  Copyright (c) 2013 Michael Henry Pantaleon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Util)
+
+@end
---
+++ Sample/FlipViewControllerDemo/FlipViewControllerDemo/UIView+Util.m
@@ -0,0 +1,13 @@
+//
+//  UIView+Util.m
+//  FlipViewControllerDemo
+//
+//  Created by demon on 6/8/13.
+//  Copyright (c) 2013 Michael Henry Pantaleon. All rights reserved.
+//
+
+#import "UIView+Util.h"
+
+@implementation UIView (Util)
+
+@end
